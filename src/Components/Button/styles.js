873d8cd2b33a1c63f09e1e47e22b6695615f9e1a
김민선 <alstjs4739@kookmin.ac.kr>
2023-08-19 18:49:23 +0900

    feat. 헤더 컴포넌트 구현

--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -3,9 +3,12 @@ import styled from "styled-components";
 export const Button = styled.div`
   width: ${(props) => props.width || "20px"};
   height: ${(props) => props.height || "5px"};
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background-color: #83dcb7;
   text-align: center;
-  font-size: 20px;
+  font-size: ${(props) => props.fontSize || "20px"};
   font-weight: 600;
   border-radius: 10px;
 `;
