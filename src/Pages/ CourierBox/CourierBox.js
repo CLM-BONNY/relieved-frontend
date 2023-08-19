70a2bb2a58dbbfccf1be7436359e53669e12986c
김민선 <alstjs4739@kookmin.ac.kr>
2023-08-19 21:58:38 +0900

    feat. 안심 택배함 페이지 구현

---
+++ src/Pages/ CourierBox/CourierBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+import * as style from "./styles.js";
+import Header from "../../Components/Header/Header.js";
+import Map from "../../Components/Map/Map.js";
+
+export default function CourierBox() {
+  return (
+    <>
+      <Header title={"안심 택배함"} />
+      <Map />
+      <style.imgBtn>
+        <img
+          src={process.env.PUBLIC_URL + "/Images/CourierBox/CurrentIcon.svg"}
+        />
+      </style.imgBtn>
+    </>
+  );
+}
