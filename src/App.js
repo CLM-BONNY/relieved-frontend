eeb0dd3c2b5375f441299a61d9ffaa98864121d7
김민선 <alstjs4739@kookmin.ac.kr>
2023-08-20 00:18:20 +0900

    feat. 페이지 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import WayBackHome from "./Pages/WayBackHome/WayBackHome";
+import CourierBox from "./Pages/CourierBox/CourierBox";
+import DangerousPlace from "./Pages/DangerousPlace/DangerousPlace";
+import Report from "./Pages/Report/Report";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/wayBackHome" element={<WayBackHome />} />
+          <Route path="/courierBox" element={<CourierBox />} />
+          <Route path="/dangerousPlace" element={<DangerousPlace />} />
+          <Route path="/report" element={<Report />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
