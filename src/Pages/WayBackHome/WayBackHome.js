718e5b30247772c3479ef70aa4a46c527d40fb8b
cg10036 <cg10036@gmail.com>
2023-08-20 03:41:04 +0900

    popup to iframe

--- src/Pages/WayBackHome/WayBackHome.js
+++ src/Pages/WayBackHome/WayBackHome.js
@@ -12,42 +12,85 @@ const search = (callback) => {
       if (callback) callback(data);
     },
   });
-  search.open();
+  // search.open();
+  search.embed(document.getElementById("search"));
 };
 
 export default function WayBackHome() {
   const title = "안심 귀갓길";
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
+  const [searchMode, setSearchMode] = useState("");
 
   return (
     <>
       <Header title={title} />
-      <style.location>
-        <img
-          src={process.env.PUBLIC_URL + "/Images/WayBackHome/DepartIcon.svg"}
-        />
-        <Input
-          placeholder={"출발지를 입력해 주세요"}
-          onClick={() => {
-            search((data) => setFrom(data.address));
-          }}
-          value={from}
-        />
-      </style.location>
-      <style.location>
-        <img
-          src={process.env.PUBLIC_URL + "/Images/WayBackHome/ArriveIcon.svg"}
-        />
-        <Input
-          placeholder={"도착지를 입력해 주세요"}
-          onClick={() => {
-            search((data) => setTo(data.address));
-          }}
-          value={to}
-        />
-      </style.location>
-      <Map type="way_back_home" from={from} to={to} title={title} />
+      <div style={{ display: searchMode ? "block" : "none" }}>
+        <style.location>
+          <div
+            id="search"
+            style={{
+              width: "100%",
+            }}
+          >
+            <img
+              src="//t1.daumcdn.net/postcode/resource/images/close.png"
+              id="btnFoldWrap"
+              style={{
+                cursor: "pointer",
+                position: "absolute",
+                right: "20px",
+                top: "60px",
+                "z-index": "1",
+              }}
+              onClick={() => {
+                document.querySelector("#search > div").remove();
+                setSearchMode(false);
+              }}
+              alt="접기 버튼"
+            ></img>
+          </div>
+        </style.location>
+      </div>
+      <div
+        style={{
+          display: searchMode ? "none" : "block",
+        }}
+      >
+        <style.location>
+          <img
+            src={process.env.PUBLIC_URL + "/Images/WayBackHome/DepartIcon.svg"}
+          />
+          <Input
+            placeholder={"출발지를 입력해 주세요"}
+            onClick={() => {
+              setSearchMode(true);
+              search((data) => {
+                setSearchMode(false);
+                setFrom(data.address);
+              });
+            }}
+            value={from}
+          />
+        </style.location>
+        <style.location>
+          <img
+            src={process.env.PUBLIC_URL + "/Images/WayBackHome/ArriveIcon.svg"}
+          />
+          <Input
+            placeholder={"도착지를 입력해 주세요"}
+            onClick={() => {
+              setSearchMode(true);
+              search((data) => {
+                setSearchMode(false);
+                setTo(data.address);
+              });
+            }}
+            value={to}
+          />
+        </style.location>
+        <Map type="way_back_home" from={from} to={to} title={title} />
+      </div>
       <Footer title={title} />
     </>
   );
